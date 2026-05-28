b1dad0b6134b3900f856f5fd6b3571ed307825d8
YuTing <jenny910223@gmail.com>
2026-05-28 15:08:49 +0800

    fix: enable headless mode and CI options for Chrome

--- fetch_shareholders_meetings.py
+++ fetch_shareholders_meetings.py
@@ -16,7 +16,10 @@ def fetch_all_shareholders_meetings():
     # 瀏覽器設定
     options = Options()
     # 💡 建議：本機開發測試時，先把下面這行註解掉，讓你看得到瀏覽器自動點擊的過程
-    # options.add_argument('--headless') 
+    options.add_argument('--headless') 
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
 
